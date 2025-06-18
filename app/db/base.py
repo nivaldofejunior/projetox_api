c490d0abc040e50d20903254c21f04630ae20047
nivaldo junior <nivaldofe.jr@gmail.com>
2025-06-18 13:23:40 -0400

    ajuste no import

--- app/db/base.py
+++ app/db/base.py
@@ -1,9 +1,4 @@
+# app/db/base.py
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
-
-# Importa os modelos diretamente para garantir que as tabelas sejam registradas
-from app.models.cliente import Cliente
-from app.models.advogado import Advogado
-from app.models.caso import Caso
-from app.models.mensagem import Mensagem
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,5 @@
+# app/models/__init__.py
 from app.models.cliente import Cliente
 from app.models.advogado import Advogado
 from app.models.caso import Caso
 from app.models.mensagem import Mensagem
-
-# Isso garante que os modelos estejam registrados no metadata do SQLAlchemy
