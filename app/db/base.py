46d9903e6732587c03d89403c1d42952db80b72c
nivaldo junior <nivaldofe.jr@gmail.com>
2025-06-18 13:11:15 -0400

    ajustando o import do banco

--- app/db/base.py
+++ app/db/base.py
@@ -1,14 +1,9 @@
 from sqlalchemy.orm import declarative_base
-from fastapi import APIRouter
 
 Base = declarative_base()
 
-# Importa os modelos para registrar no metadata
-def import_models():
-    from app.models.cliente import Cliente
-    from app.models.advogado import Advogado
-    from app.models.caso import Caso
-    from app.models.mensagem import Mensagem
-
-router = APIRouter()
-
+# Importa os modelos diretamente para garantir que as tabelas sejam registradas
+from app.models.cliente import Cliente
+from app.models.advogado import Advogado
+from app.models.caso import Caso
+from app.models.mensagem import Mensagem
